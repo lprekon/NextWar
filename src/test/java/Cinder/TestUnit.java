5c3ae43291793a7228ae7e511e4d50a4e2a36226
John O'kane <jtommyok8@gmail.com>
2020-03-23 20:41:35 -0400

    adds test for empty unit name and status

--- src/test/java/Cinder/TestUnit.java
+++ src/test/java/Cinder/TestUnit.java
@@ -1,12 +1,14 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
-class MyFirstJUnitJupiterTests {
+class TestUnit {
 
     @Test
-    void addition() {
-        assertEquals(2, 2);
+    void gettersWithEmptyUnit() {
+        Unit emptyUnit = new Unit();
+        assertEquals("Empty Unit", emptyUnit.getName());
+        assertEquals(UnitStatus.DEAD, emptyUnit.getStatus());
     }
 
 }
