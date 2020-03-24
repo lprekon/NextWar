080405af890b6a9ce3da4222dcc80de682dbc178
John O'kane <jtommyok8@gmail.com>
2020-03-24 12:21:54 -0400

    adds stub files for UnitStack class and tests

---
+++ src/test/java/Cinder/TestUnitStack.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestUnitStack{
+	
+}
