ce532454e4be14fd7cacd1a5445983890d27bb69
John O'kane <jtommyok8@gmail.com>
2020-03-28 23:33:58 -0400

    adds BattleReport class

---
+++ src/main/java/Cinder/BattleReport.java
@@ -0,0 +1,16 @@
+public class BattleReport{
+	public int attackerStepLoss=0;
+	public int defenderStepLoss=0;
+	public boolean defenderRetreat=false;
+	public double powerRatio=0;
+	public int efficiancyDifference=0;
+	public int diceRollModifier=0;
+	public int netColumnShift=0;
+	public UnitStack attacker;
+	public UnitStack defender;
+
+	public BattleReport(UnitStack attacker, UnitStack defender){
+		this.attacker = attacker;
+		this.defender = defender;
+	}
+}
