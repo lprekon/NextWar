d460ba35fd825d3bf8d505d92dfb263890a36df6
John O'kane <jtommyok8@gmail.com>
2020-03-28 23:50:22 -0400

    adds TerrainType enum

---
+++ src/main/java/Cinder/TerrainType.java
@@ -0,0 +1,18 @@
+public enum TerrainType
+{
+	FLAT ("FLAT"), FLAT_WOODS ("FLAT_WOODS"), ROUGH ("ROUGH"), ROUGH_WOODS ("ROUGH_WOODS"), 
+	MARSH ("MARSH"), HIGHLAND ("HIGHLAND"), HIGHLAND_WOODS ("HIGHLAND_WOODS"), 
+	MOUNTAIN ("MOUNTAIN"), URBAN ("URBAN");
+
+	private String name;
+
+	UnitStatus(String name)
+	{
+		this.name = name;
+	}
+
+	public String toString()
+	{
+		return name;
+	}
+}
