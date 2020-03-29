c7723a56e0cbddfb323a1b8e9e9de5e9c12b61a5
John O'kane <jtommyok8@gmail.com>
2020-03-29 00:07:20 -0400

    adds part of attack() method

--- src/main/java/Cinder/UnitStack.java
+++ src/main/java/Cinder/UnitStack.java
@@ -1,7 +1,33 @@
 public class UnitStack{
 	protected Unit[] stack;
+	private Tile location;
+	public static Map<double, BattleReport[][][]> CRT = null;
+
+	public static void buildCRT(){
+
+	}
 
 	public UnitStack(Unit[] units){
 		this.stack = units;
+	}
+
+	public BattleReport attack(UnitStack defender, Supplier<int> die){
+		double powerRatio = getAttackPower()/defender.getDefensePower();
+	}
+
+	public int getAttackPower(){
+		int p = 0;
+		for (Unit u:stack){
+			p += u.getAttack();
+		}
+		return p;
+	}
+
+	public int getDefensePower(){
+		int p = 0;
+		for(Unit u:stack){
+			p += u.getDefense();
+		}
+		return p;
 	}
 }
