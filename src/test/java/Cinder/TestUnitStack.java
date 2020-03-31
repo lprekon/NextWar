66604af4adfb23ed8291289710d9e196425e0134
John O'kane <jtommyok8@gmail.com>
2020-03-31 06:00:47 -0400

    adds several tests for CombatResultsTable.getResult

--- src/test/java/Cinder/TestUnitStack.java
+++ src/test/java/Cinder/TestUnitStack.java
@@ -1,10 +1,26 @@
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 public class TestUnitStack{
 	
+	static Stream<Arguments> testCombatResultsFactory(){
+		return Stream.of(
+			Arguments.of(1.0, TerrainType.FLAT, 0, 0, new BattleResult(0, 1, true, false)),
+			Arguments.of(2.0, TerrainType.URBAN, 9, -2, new BattleResult(3, 1, false, false)),
+			Arguments.of(4.0, TerrainType.HIGHLAND, 3, 2, new BattleResult(1, 2, true, true)));
+	}
+
+	@ParameterizedTest
+	@MethodSource("testCombatResultsFactory")
+	void testCRT(double powerRatio, TerrainType tType, int modifiedRoll, int columnShift, BattleResult correctAnswer){
+		assertEquals(correctAnswer, UnitStack.CombatResultsTable.getResult(powerRatio, tType, modifiedRoll, columnShift));
+	}
 }
