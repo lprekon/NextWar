7abb6fb411a2255dccb38cb9eb0eed30e5e1d326
John O'kane <jtommyok8@gmail.com>
2020-04-01 14:53:59 -0400

    Adds several tests

--- src/test/java/Cinder/TestUnitStack.java
+++ src/test/java/Cinder/TestUnitStack.java
@@ -13,9 +13,15 @@ public class TestUnitStack{
 	
 	static Stream<Arguments> testCombatResultsFactory(){
 		return Stream.of(
-			Arguments.of(1.0, TerrainType.FLAT, 0, 0, new BattleResult(0, 1, true, false)),
-			Arguments.of(2.0, TerrainType.URBAN, 9, -2, new BattleResult(3, 1, false, false)),
-			Arguments.of(4.0, TerrainType.HIGHLAND, 3, 2, new BattleResult(1, 2, true, true)));
+			Arguments.of(1.0, TerrainType.FLAT, 0, 0, new BattleResult(0, 1, true, false, 3, 6)),
+			Arguments.of(2.0, TerrainType.URBAN, 9, -2, new BattleResult(3, 1, false, false, 12, 2)),
+			Arguments.of(4.0, TerrainType.HIGHLAND, 3, 2, new BattleResult(1, 2, true, true, 6, 10)),
+			Arguments.of(4.1, TerrainType.HIGHLAND_WOODS, 3, 2, new BattleResult(0, 2, true, true, 5, 10)),
+			Arguments.of(1.5, TerrainType.ROUGH, -2, 2, new BattleResult(1, 3, true, false, 1, 8)),
+			Arguments.of(.5, TerrainType.MOUNTAIN, 1, 1, new BattleResult(1, 1, false, false, 4, 3)),
+			Arguments.of(1, TerrainType.MOUNTAIN, 1, 0, new BattleResult(1, 1, false, false, 4, 3)),
+			Arguments.of(1.0/3, TerrainType.URBAN, -3, 0, new BattleResult(1, 1, true, false, 0, 0)),
+			Arguments.of(1.0/3, TerrainType.FLAT_WOODS, -3, 0, new BattleResult(0, 2, true, false, 0, 4)));
 	}
 
 	@ParameterizedTest
