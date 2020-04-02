ff811f1d1e4fae95755a0d9ccf379ce2f83f9d10
John O'kane <jtommyok8@gmail.com>
2020-04-02 17:16:27 -0400

    reorganizes tests

--- src/test/java/Cinder/TestUnitStack.java
+++ src/test/java/Cinder/TestUnitStack.java
@@ -24,9 +24,48 @@ public class TestUnitStack{
 			Arguments.of(1.0/3, TerrainType.FLAT_WOODS, -3, 0, new BattleResult(0, 2, true, false, 0, 4)));
 	}
 
-	@ParameterizedTest
-	@MethodSource("testCombatResultsFactory")
-	void testCRT(double powerRatio, TerrainType tType, int modifiedRoll, int columnShift, BattleResult correctAnswer){
-		assertEquals(correctAnswer, UnitStack.CombatResultsTable.getResult(powerRatio, tType, modifiedRoll, columnShift));
+	// @ParameterizedTest
+	// @MethodSource("testCombatResultsFactory")
+	// void testCRT(double powerRatio, TerrainType tType, int modifiedRoll, int columnShift, BattleResult correctAnswer){
+	// 	assertEquals(correctAnswer, UnitStack.CombatResultsTable.getResult(powerRatio, tType, modifiedRoll, columnShift));
+	// }
+
+	@Test
+	void simpleCombat(){
+		assertEquals(new BattleResult(1, 2, true, false, 3, 7), UnitStack.CombatResultsTable.getResult(5.0, TerrainType.URBAN, 0, 0));
+	}
+
+	@Test
+	void terrainShift_5_0(){
+		assertEquals(new BattleResult(1, 2, true, false, 3, 7), UnitStack.CombatResultsTable.getResult(5.0, TerrainType.URBAN, 0, 0));
+		assertEquals(new BattleResult(0, 1, true, false, 3, 8), UnitStack.CombatResultsTable.getResult(5.0, TerrainType.MOUNTAIN, 0, 0));
+		assertEquals(new BattleResult(0, 2, true, false, 3, 9), UnitStack.CombatResultsTable.getResult(5.0, TerrainType.HIGHLAND, 0, 0));
+		assertEquals(new BattleResult(1, 3, true, true, 3, 10), UnitStack.CombatResultsTable.getResult(5.0, TerrainType.ROUGH, 0, 0));
+		assertEquals(new BattleResult(1, 2, true, true, 3, 11), UnitStack.CombatResultsTable.getResult(5.0, TerrainType.FLAT, 0, 0));
+	}
+
+	@Test
+	void columnShift(){
+		assertEquals(new BattleResult(0, 1, true, false, 3, 6), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, 0, 0));
+		assertAll(()->assertEquals(new BattleResult(1, 2, true, false, 3, 7), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, 0, 1)),
+			()->assertEquals(new BattleResult(0, 1, true, false, 3, 5), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, 0, -1)));
+	}
+
+	@Test
+	void fullFlat(){
+		assertAll(()->assertEquals(new BattleResult(0, 1, true, false, 3, 6), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, 0, 0)),
+			()->assertEquals(new BattleResult(1, 1, true, false, 5, 6), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, 2, 0)),
+			()->assertEquals(new BattleResult(0, 2, true, false, 1, 6), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, -2, 0)),
+			()->assertEquals(new BattleResult(1, 2, true, false, 3, 7), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, 0, 1)),
+			()->assertEquals(new BattleResult(1, 1, true, false, 3, 4), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.FLAT, 0, -2)));
+	}
+
+	@Test
+	void fullUrban(){
+		assertAll(()->assertEquals(new BattleResult(1, 1, false, false, 3, 2), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.URBAN, 0, 0), "Start"),
+			()->assertEquals(new BattleResult(1, 2, false, false, 5, 2), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.URBAN, 1, 0), "DRM +"),
+			()->assertEquals(new BattleResult(1, 1, true, false, 1, 2), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.URBAN, -2, 0), "DRM -"),
+			()->assertEquals(new BattleResult(1, 1, true, false, 3, 3), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.URBAN, 0, 1), "Column +"),
+			()->assertEquals(new BattleResult(1, 1, false, false, 3, 1), UnitStack.CombatResultsTable.getResult(1.0, TerrainType.URBAN, 0, -1), "Column -"));
 	}
 }
